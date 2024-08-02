1d3b7e3bf6ee2b9ea5e43e91d84501e609fa0d8a
javohir-abdunabiyev <jahaajahaaa2@gmail.com>
2024-08-02 18:29:41 +0500

    aa

--- src/components/persons.ts
+++ src/components/persons.ts
@@ -5,7 +5,7 @@ export function popularPersons(item: any) {
     img.className = "act_img"
 
     img.onclick = () => {
-        location.assign("/src/pages/actors/")
+        location.assign(`/src/pages/actors/?id=${item.id}`)
     }
 
     return img
